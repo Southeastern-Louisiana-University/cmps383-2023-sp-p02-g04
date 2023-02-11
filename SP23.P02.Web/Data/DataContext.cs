dab7a72270e709668e88a6a60735ebaf6bc1f28e
JerrySelu <69907204+JerrySelu@users.noreply.github.com>
2023-02-11 13:53:18 -0600

    Revert "Setting up classes"
    This reverts commit e52eb92584df1e28dc977eeff5467532b3ee9b3f.

--- SP23.P02.Web/Data/DataContext.cs
+++ SP23.P02.Web/Data/DataContext.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
 namespace SP23.P02.Web.Data;
 
-public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
--- SP23.P02.Web/Data/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace SP23.P02.Web.Data
-{
-    public class UserRole : IdentityUserRole<int>
-    {
-    }
-}
