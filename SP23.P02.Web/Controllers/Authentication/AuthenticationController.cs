763760cc0d38315da9cca1ad738ea811a84225fd
Autumn Kennedy <autumnkennedy0714@gmail.com>
2023-02-13 15:21:42 -0600

    Post Endpoint Auth Controller

--- SP23.P02.Web/Controllers/Authentication/AuthenticationController.cs
+++ SP23.P02.Web/Controllers/Authentication/AuthenticationController.cs
@@ -1,6 +1,34 @@
-﻿namespace SP23.P02.Web.Controllers.Authentication
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P02.Web.Data;
+using SP23.P02.Web.Features.Users;
+
+namespace SP23.P02.Web.Controllers.Authentication;
+
+
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
 {
-    public class AuthenticationController
+    private readonly DataContext dataContext;
+
+    public AuthenticationController(DataContext dataContext)
     {
+        this.dataContext = dataContext;
     }
+
+    [HttpPost]
+    [Route("api/authentication/login")]
+
+    public ActionResult<LoginDto> Login(LoginDto loginDto)
+    {
+        if (IsInvalid(loginDto))
+        {
+            return BadRequest();
+        }
+    }
+
+
+
 }
